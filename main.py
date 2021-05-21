837aa414e334ce3f34fcba6099b6e4daa2ed8365
cisel-zumbul <68049174+cisel-zumbul@users.noreply.github.com>
2021-05-21 23:02:58 +0300

    Add files via upload
    Oldu da bitti Maaşallah

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import re
 
 f=open('file','r')
-map={'HALT':1,'LOAD':2,'STORE':3,'ADD':4,'SUB':5,'INC':6,'DEC':7,'XOR':8,'AND':9,'OR':'A','NOT':'B','SHL':'C','SHR':'D','NOP':'E','PUSH':'F','POP':10,'CMP':11,'JMP':12,'JZ':13,'JE':13,'JNZ':14,'JNE':14,'JC':15,'JNC':16, 'JA':17,'JAE':18,'JB':19,
+map={'HALT':"1",'LOAD':"2",'STORE':"3",'ADD':"4",'SUB':"5",'INC':"6",'DEC':"7",'XOR':"8",'AND':"9",'OR':'A','NOT':'B','SHL':'C','SHR':'D','NOP':'E','PUSH':'F','POP':"10",'CMP':"11",'JMP':"12",'JZ':"13",'JE':"13",'JNZ':"14",'JNE':"14",'JC':"15",'JNC':"16", 'JA':"17",'JAE':"18",'JB':"19",
      'JBE':'1A','READ':'1B','PRINT':'1C'}
-registers={'PC':0,'A':1,'B':2,'C':3,'D':4,'E':5,'S':6}
+registers={'PC':"0",'A':"1",'B':"2",'C':"3",'D':"4",'E':"5",'S':"6"}
 labels={}
 
 
@@ -23,9 +23,9 @@ f=open('file','r')
 
 
 for lines in f:
-    opcode = -1
-    addrmode = -1
-    operand = -1
+    opcode = "-1"
+    addrmode = "-1"
+    operand = "-1"
     word= lines.split(" ")
     for token in word:
         labelmi=re.search('\:',token)
@@ -39,16 +39,16 @@ for lines in f:
             opcode=map[token]
             continue
         if token in labels: #labelsa
-            addrmode=0
+            addrmode="0"
             operand=labels[token]
             continue
         if token in registers: #registerlardan biriyse
-            addrmode=1
+            addrmode="1"
             operand=registers[token]
             continue
         tirnak=re.search('\'',token)
         if tirnak: #tırnak içindeyse
-            addrmode=0
+            addrmode="0"
             token = token[1:2]
             asci = format(ord(token), "x")
             operand=asci
@@ -58,26 +58,38 @@ for lines in f:
             if len(token) is 3:  #  [B] gibi
                 token = token[1:2]
                 val=registers[token]
-                addrmode = 2
+                addrmode = "2"
                 operand=val
                 continue
             else:  #  [1234] gibi
                 closeb=token.find('\]')
                 memo=token[1:closeb]
-                addrmode = 3
+                addrmode = "3"
                 operand=memo
                 continue
-        #hiçbiri değilse normal sayıdır  load 0004 
-        addrmode=0
+        #hiçbiri değilse normal sayıdır  load 0004
+        addrmode="0"
+        operand=token
         if operand is -1:
             operand=token
 
     print(opcode,addrmode,operand)
 
+    if opcode=="1":
+        adrrmode="0"
+        operand="0"
 
+    if opcode=="-1":
+        continue
 
+    opcode1 = int(opcode, 16)
+    addrmode1 = int(addrmode, 16)
+    operand1 = int(operand, 16)
 
-
-
-
-
+    bopcode = format(opcode1, '06b')
+    baddrmode = format(addrmode1, '02b')
+    boperand = format(operand1, '016b')
+    bin = '0b' + bopcode + baddrmode + boperand
+    ibin = int(bin[2:], 2);
+    instr = format(ibin, '06x')
+    print(instr)
